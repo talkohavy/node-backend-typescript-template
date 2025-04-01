bfce666f2c8c0860e1a985bd5e78a70e32e98b5d
Tal Kohavy <talkohavy@gmail.com>
2025-04-01 18:23:49 +0300

    eslint sort

--- src/initServer/loadEnvVariables.ts
+++ src/initServer/loadEnvVariables.ts
@@ -1,9 +1,9 @@
+import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const pathToEnvFile = path.resolve(__dirname, '../../../../.env');
+const pathToEnvFile = path.resolve(__dirname, '../../.env');
 
 dotenv.config({ path: pathToEnvFile });
