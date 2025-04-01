75848cf42ce4b4f9aac40c443283bbf8da1f530f
Tal Kohavy <talkohavy@gmail.com>
2025-04-01 17:37:35 +0300

    turned the project into a backend template

--- apps/frontend/src/lib/syntaxHighlighter/index.ts
+++
@@ -1,8 +0,0 @@
-import hljs from 'highlight.js';
-import { typescriptLanguageToRegister } from './languages/typescript';
-import { javascriptLanguageToRegister } from './languages/javascript';
-import './styles/vscode-dark.css';
-import './styles/1c-light.css';
-
-hljs.registerLanguage('javascript', javascriptLanguageToRegister);
-hljs.registerLanguage('typescript', typescriptLanguageToRegister);
