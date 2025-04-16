f6fd6cf793b557e7ddd715a756b446079b1e0e6f
Tal Kohavy <talkohavy@gmail.com>
2025-04-16 17:47:59 +0300

    initServer is in root

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { startServer } from './initServer/index.js';
-
-startServer();
---
+++ src/initServer.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { configuration } from './configurations/index.js';
+import { CallContextMiddleware } from './lib/call-context/call-context.middleware.js';
+import { initCallContextService } from './lib/call-context/call-context.service.js';
+import { initConfigService } from './lib/config/config.service.js';
+import { initLoggerService } from './lib/logger/logger.service.js';
+import { attachBaseMiddlewares } from './middlewares/attachBaseMiddlewares.js';
+import { attachErrorMiddlewares } from './middlewares/attachErrorMiddlewares.js';
+import { attachBooksModule } from './modules/books/books.module.js';
+import { attachServerSentEventModule } from './modules/serverSentEvents/serverSentEvents.module.js';
+import { attachUsersModule } from './modules/users/users.module.js';
+
+export async function startServer() {
+  const configService = initConfigService(configuration());
+  const callContextService = initCallContextService();
+  const logger = initLoggerService(configService, callContextService);
+  const callContextMiddleware = new CallContextMiddleware(callContextService, configService);
+
+  const app = express();
+
+  attachBaseMiddlewares({ app });
+
+  app.use(callContextMiddleware.use.bind(callContextMiddleware));
+
+  attachServerSentEventModule(app);
+  attachUsersModule(app);
+  attachBooksModule(app);
+
+  attachErrorMiddlewares({ app });
+
+  app.listen(process.env.BACKEND_PORT, () => logger.log(`server started on port ${process.env.BACKEND_PORT}`));
+}
+
+startServer();
