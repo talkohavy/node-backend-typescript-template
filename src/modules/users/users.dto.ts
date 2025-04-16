7ecde95770ead1b13217cb34ab2cb1fc3a3450f1
Tal Kohavy <talkohavy@gmail.com>
2025-04-16 21:42:30 +0300

    updated createUserSchema

--- src/modules/users/users.dto.ts
+++ src/modules/users/users.dto.ts
@@ -1,9 +1,7 @@
 import Joi from 'joi';
 
 export const createUserSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  // password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-  // repeat_password: Joi.ref('password'),
-  // birth_year: Joi.number().integer().min(1900).max(2013),
-  // email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+  name: Joi.string().min(1).max(30).required(),
+  age: Joi.number().integer().min(0).max(120).optional(),
+  email: Joi.string().email().optional(),
 });
