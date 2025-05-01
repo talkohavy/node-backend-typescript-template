ef1e8ce7f2c0fd1e57e92aad63687bd094ef15a9
Tal Kohavy <talkohavy@gmail.com>
2025-05-01 09:35:59 +0300

    joi validation middleware now throws BadRequestError instead of responding

--- src/middlewares/attachJoiMiddleware.ts
+++ src/middlewares/attachJoiMiddleware.ts
@@ -1,17 +1,17 @@
 import type { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
-import { STATUS_CODES } from '../common/constants';
+import { BadRequestError } from '../lib/Errors';
 import { logger } from '../lib/logger';
 
 export function attachJoiMiddleware(validationSchema: Joi.ObjectSchema<any>): any {
-  return function validateUsingJoi(req: Request, res: Response, next: NextFunction) {
+  return function validateUsingJoi(req: Request, _res: Response, next: NextFunction) {
     const { body } = req;
 
     const { error } = validationSchema.validate(body);
 
     if (error) {
       logger.log('Validation error', error);
-      return res.status(STATUS_CODES.BAD_REQUEST).json({ message: error.message });
+      throw new BadRequestError(error.message);
     }
 
     next();
