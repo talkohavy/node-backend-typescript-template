88f8206f065c88e125e43ce65f14189389f37340
Tal Kohavy <talkohavy@gmail.com>
2025-08-17 19:46:36 +0300

    users: no more controllers

--- src/modules/users/controllers/dto/updateUserSchema.dto.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-export const updateUserSchema = Joi.object({
-  name: Joi.string().min(1).max(30).optional(),
-  age: Joi.number().integer().min(0).max(120).optional(),
-  email: Joi.string().email().optional(),
-})
-  .or('name', 'age', 'email')
-  .required();
--- src/modules/users/controllers/users.controller.ts
+++
@@ -1,9 +0,0 @@
-import { UsersCrudController } from './users-crud.controller';
-
-export class UsersController {
-  constructor(private readonly usersCrudController: UsersCrudController) {}
-
-  attachRoutes() {
-    this.usersCrudController.attachRoutes();
-  }
-}
