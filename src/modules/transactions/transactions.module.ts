d8f1d283ee059e2e6a643b47421e0c71dbc96dcb
Tal Kohavy <talkohavy@gmail.com>
2025-09-27 17:01:26 +0300

    converted attachTransactionsModule to TransactionsModule

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,8 +1,21 @@
 import { Application } from 'express';
 import { TransactionsController } from './transactions.controller';
 
-export function attachTransactionsModule(app: Application) {
-  const controller = new TransactionsController(app);
+export class TransactionsModule {
+  private static instance: TransactionsModule;
 
-  controller.attachRoutes();
+  private constructor() {}
+
+  static getInstance(): TransactionsModule {
+    if (!TransactionsModule.instance) {
+      TransactionsModule.instance = new TransactionsModule();
+    }
+    return TransactionsModule.instance;
+  }
+
+  attachController(app: Application): void {
+    const controller = new TransactionsController(app);
+
+    controller.attachRoutes();
+  }
 }
