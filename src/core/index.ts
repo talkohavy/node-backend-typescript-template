73635cc5a66dca0cc137f184d4f9119804560345
Tal Kohavy <talkohavy@gmail.com>
2025-09-27 23:12:44 +0300

    export ModuleRegistry from core index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,10 @@
+// singleton services initializer
+export { bootstrap } from './bootstrap';
+
+// Module registry initializer
+export { ModuleRegistry } from './moduleRegistry';
+
 // Core services already initialized
 export { callContextService } from './initCallContextService';
 export { logger } from './initLoggerService';
 export { configService } from './initConfigService';
-
-// Bootstrap function
-export { bootstrap } from './bootstrap';
