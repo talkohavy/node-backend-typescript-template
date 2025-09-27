e52797be4eda2ef16335f91c3c206f4676967358
Tal Kohavy <talkohavy@gmail.com>
2025-09-27 22:36:38 +0300

    typescript: added important user friendly comments

--- src/@types/json-easy-strip.d.ts
+++ src/@types/json-easy-strip.d.ts
@@ -1,3 +1,5 @@
+// Add new module declaration in Script Mode
+
 declare module 'json-easy-strip' {
   function requireJSON(path: string): any;
   export default requireJSON;
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,5 @@
+// Add module augmentations here
+
 declare module 'express' {
   export interface Request {
     user?: Record<string, any>;
