9c75f0868ca6e53a46d69eaf472e23bc03f2e9f0
Tal Kohavy <talkohavy@gmail.com>
2025-09-27 19:48:02 +0300

    @types: fixed types for global.d.ts

---
+++ src/@types/json-easy-strip.d.ts
@@ -0,0 +1,4 @@
+declare module 'json-easy-strip' {
+  function requireJSON(path: string): any;
+  export default requireJSON;
+}
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,7 @@
-declare module 'json-easy-strip' {
-  function requireJSON(path: string): any;
-  export default requireJSON;
-}
-
 declare module 'express' {
   export interface Request {
     user?: Record<string, any>;
   }
 }
+
+export {};
