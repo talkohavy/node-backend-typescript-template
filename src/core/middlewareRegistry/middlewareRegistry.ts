05d83d47ab010c745471e8684f13af349582b000
Tal Kohavy <talkohavy@gmail.com>
2025-10-01 07:04:23 +0300

    need to export CallContextMiddleware

--- src/core/middlewareRegistry/middlewareRegistry.ts
+++ src/core/middlewareRegistry/middlewareRegistry.ts
@@ -1,7 +1,7 @@
 import { Application } from 'express';
 import { postUseMiddleware } from '../../common/utils/postUseMiddleware';
 import { preUseMiddleware } from '../../common/utils/preUseMiddleware';
-import { CallContextMiddleware } from '../../lib/call-context/call-context.middleware';
+import { CallContextMiddleware } from '../../lib/call-context';
 import { attachBaseMiddlewares } from '../../middlewares/attachBaseMiddlewares';
 import { attachErrorMiddlewares } from '../../middlewares/attachErrorMiddlewares';
 import { attachRequestIdMiddleware } from '../../middlewares/attachRequestIdMiddleware';
--- src/lib/call-context/index.ts
+++ src/lib/call-context/index.ts
@@ -1,4 +1,5 @@
 export { CallContextService, callContextService, initCallContextService } from './call-context.service';
+export { CallContextMiddleware } from './call-context.middleware';
 
 // constants
 export { RequestContext } from './logic/constants';
