cfd83091d75c44be16b96c6380fd91204a7be6fe
Tal Kohavy <talkohavy@gmail.com>
2025-11-20 07:51:59 +0200

    dummy login

--- src/initServer.ts
+++ src/initServer.ts
@@ -1,20 +1,20 @@
 import express from 'express';
+// import { TransactionsModule } from '../modules/transactions';
 import { ConfigKeys } from './configurations';
 import { initGlobalServices, initConnections, MiddlewareRegistry } from './core';
 import { ModuleRegistry } from './lib/lucky-server';
+import { BackendModule } from './modules/backend';
+// import { BooksModule } from './modules/books';
 import { HealthCheckModule } from './modules/health-check';
 import { SwaggerModule } from './modules/swagger';
 import { UsersModule } from './modules/users';
-// import { BooksModule } from './modules/books';
-// import { BackendModule } from '../../modules/backend';
-// import { TransactionsModule } from '../modules/transactions';
 
 export async function startServer() {
   const { configService, callContextService, loggerService: logger } = await initGlobalServices();
 
   await initConnections(configService);
 
-  const moduleRegistry = new ModuleRegistry([HealthCheckModule, UsersModule, SwaggerModule]); // BooksModule,BackendModule, TransactionsModule
+  const moduleRegistry = new ModuleRegistry([HealthCheckModule, BackendModule, UsersModule, SwaggerModule]); // BooksModule, BackendModule, TransactionsModule
   const middlewareRegistry = new MiddlewareRegistry(callContextService);
 
   const app = express();
