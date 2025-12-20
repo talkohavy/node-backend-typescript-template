3997b5da4a95b22df27592bc2b5190bb2a9a1947
Tal Kohavy <talkohavy@gmail.com>
2025-12-20 18:04:45 +0200

    updated mockApp

--- src/mockApp.ts
+++ src/mockApp.ts
@@ -1,22 +1,7 @@
-import type { LoggerService } from './lib/logger-service';
 import { buildApp } from './app';
-import { initConnections, initGlobalServices } from './core';
 
 export async function buildMockApp() {
-  const { configService } = await initGlobalServices();
-
-  const logger = {
-    log: (_message: string) => {},
-    debug: (_message: string) => {},
-    info: (_message: string) => {},
-    warn: (_message: string) => {},
-    error: (_message: string) => {},
-    fatal: (_message: string) => {},
-  } as LoggerService;
-
-  await initConnections(configService);
-
-  const app = await buildApp({ logger });
+  const app = await buildApp();
 
   return app;
 }
